80e30643f0cdf6fd0dd3157577212aad480a50fb
gyum8651 <gyum8651@gmail.com>
2024-11-06 21:56:50 +0900

    코드 최적화 및 프로젝트 모듈화

--- routes/logs.py
+++ routes/logs.py
@@ -1,28 +1,28 @@
 # routes/logs.py
 from flask import Blueprint, render_template, request
 from models import SimulationData, Predictions
-from datetime import datetime
+from datetime import datetime, timedelta
 
 logs_bp = Blueprint('logs', __name__)
 
 
 @logs_bp.route('/logs', methods=['GET', 'POST'])
 def logs():
-    # 기본 날짜를 오늘로 설정
+    # 기본 날짜를 오늘로 설정하고, 선택된 날짜가 있으면 해당 날짜 사용
     selected_date = request.form.get("date", datetime.now().strftime('%Y-%m-%d'))
 
-    # 선택한 날짜의 데이터를 필터링하여 조회
+    # 시작 및 종료 날짜를 설정 (선택된 날짜의 하루 동안의 데이터)
     start_date = datetime.strptime(selected_date, '%Y-%m-%d')
-    end_date = start_date.replace(hour=23, minute=59, second=59)
+    end_date = start_date + timedelta(days=1)  # 다음 날 자정 전까지를 포함
 
     simulation_data = SimulationData.query.filter(
         SimulationData.observation_time >= start_date,
-        SimulationData.observation_time <= end_date
+        SimulationData.observation_time < end_date
     ).all()
 
     predictions_data = Predictions.query.filter(
         Predictions.observation_time >= start_date,
-        Predictions.observation_time <= end_date
+        Predictions.observation_time < end_date
     ).all()
 
     return render_template('logs.html',
