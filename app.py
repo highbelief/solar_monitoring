2d0d0fe9d9b2fb52f0ce449e73e06f70b2af74f9
gyum8651 <gyum8651@gmail.com>
2024-11-07 11:08:25 +0900

    코드 최적화

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 # app.py
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for  # redirect와 url_for을 추가로 임포트
 from config import Config
 from models import db
-from routes import init_app
+from routes import init_app  # 블루프린트 초기화 함수 가져오기
+
 app = Flask(__name__)
 app.config.from_object(Config)
 db.init_app(app)
@@ -12,11 +13,10 @@ init_app(app)
 
 # 기본 경로 리디렉션 설정 (대시보드 페이지로 이동)
 @app.route('/')
-def home():
-    return redirect(url_for('dashboard.dashboard'))  # 정확한 경로 지정
+def index():
+    return redirect(url_for('dashboard.dashboard'))
 
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # 데이터베이스 테이블 생성 (필요 시)
-    app.run(host='0.0.0.0', port=8081, debug=True)  # 원하는 포트번호로 변경
-
+    app.run(debug=True)
